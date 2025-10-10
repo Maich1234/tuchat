dee7c0aa9caacee17265c8f1eae0c829eaf96869
Maich1234 <mainamichael2849@gmail.com>
2025-10-10 10:58:30 +0300

    Add fix in messages

--- backend/src/controllers/messages/sendmessage.conroller.js
+++ backend/src/controllers/messages/sendmessage.conroller.js
@@ -1,14 +1,23 @@
 import cloudinary from "../../lib/cloudinary.js";
 import Message from "../../models/message.model.js";
 
-export default async function sendMessage(req, res){
-    try{
-        const {text, image} = req.body;
-        const {id:recieverId} = req.params;
+export default async function sendMessage(req, res) {
+    try {
+        const { text, image } = req.body;
+        const { id: recieverId } = req.params;
         const senderId = req.user._id;
-
+        if (!text && !image) {
+            return res.status(400).json({ message: "Text or image is required." });
+        }
+        if (senderId.equals(recieverId)) {
+            return res.status(400).json({ message: "Cannot send messages to yourself." });
+        }
+        const receiverExists = await User.exists({ _id: receiverId });
+        if (!receiverExists) {
+            return res.status(404).json({ message: "Receiver not found." });
+        }
         let imageUrl;
-        if(image){
+        if (image) {
             const uploadResponse = await cloudinary.uploader.upload(image);
             imageUrl = uploadResponse.secure_url;
         }
@@ -21,8 +30,8 @@ export default async function sendMessage(req, res){
         await newMessage.save();
         res.status(200).json(newMessage)
         //todo: realtime functionality if user is online
-    } catch(err){
+    } catch (err) {
         console.log("an error in send message controler", err)
-        return res.status(500).json({message:"Internal server error"})
+        return res.status(500).json({ message: "Internal server error" })
     }
 }
